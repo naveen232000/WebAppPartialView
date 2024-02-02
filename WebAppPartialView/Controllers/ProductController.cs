d0c0ef32e126cb63defeab7cea8c4ad4c2e68d17
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-02 15:06:38 +0530

    Add project files.

---
+++ WebAppPartialView/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppPartialView.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PAction()
+        {
+            return PartialView("ProductView");
+        }
+    }
+}
---
+++ WebAppPartialView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppPartialView.MvcApplication" Language="C#" %>
